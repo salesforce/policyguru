3dede388fbb4c9572d1422e59df8156e91e59f15
Kinnaird McQuade <kmcquade@salesforce.com>
2020-12-16 17:46:16 -0500

    Remove thangz

--- deploy_settings.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DEPLOYMENT_BUCKET="samcli-deployment-bucket-policyguru.io"
-export WEBSITE_BUCKET="website.policyguru.io"
-export S3_PREFIX="policyguru"
-export STACK_NAME="policyguru"
-export CAPABILITIES="CAPABILITY_IAM"
-export AWS_REGION="us-east-1"
-export DOMAIN_NAME="policyguru.io"
